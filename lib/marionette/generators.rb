20bfe4e780c729208dfae4ba0a7cd5cd74424960
Franck Delage <franck@web82.fr>
2016-02-24 18:09:23 +0100

    initial commit

---
+++ lib/marionette/generators.rb
@@ -0,0 +1,7 @@
+require "marionette/generators/version"
+
+module Marionette
+  module Generators
+    # Your code goes here...
+  end
+end
---
+++ lib/marionette/generators/version.rb
@@ -0,0 +1,5 @@
+module Marionette
+  module Generators
+    VERSION = "0.1.0"
+  end
+end
