0e99679e90a3dbaccef8d6e79d1dc10c121a1b21
Franck Delage <franck@web82.fr>
2016-02-25 11:23:19 +0100

    add generator_spec gem and begin generator spec

--- Gemfile
+++ Gemfile
@@ -1,4 +1,8 @@
 source 'https://rubygems.org'
 
+group :test do
+  gem 'generator_spec'
+end
+
 # Specify your gem's dependencies in marionette-generators.gemspec
 gemspec
--- spec/marionette/generators_spec.rb
+++ spec/marionette/generators_spec.rb
@@ -1,11 +1,31 @@
-require 'spec_helper'
+require 'generator_spec'
+require 'generators/marionette/app/app_generator'
 
-describe Marionette::Generators do
+describe Marionette::Generators::AppGenerator, type: :generator do
+  destination File.expand_path("../../tmp", __FILE__)
+  arguments %w(Dummyapp)
+
+  before(:all) do
+    prepare_destination
+    touch_files
+    run_generator
+  end
+
   it 'has a version number' do
     expect(Marionette::Generators::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'creates files' do
+    assert_file "app/assets/templates/layouts/layout.jst.skim"
   end
+
+  def touch_files
+    dest = File.expand_path("../../tmp", __FILE__)
+    %w(Gemfile Bowerfile).each do |file|
+      FileUtils.touch File.join(dest, file)
+    end
+    FileUtils.mkdir_p File.join(dest, "app", "assets", "javascripts")
+    FileUtils.touch File.join(dest, "app", "assets", "javascripts", "application.js")
+  end
+
 end
